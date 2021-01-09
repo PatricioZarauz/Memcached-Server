b243d3a4dc17af21529e59465143141835b0af58
Patricio Zarauz <pwzarauz@gmail.com>
2021-01-09 17:23:59 -0300

    Added a red black tree
    This was added to the server inorder to keep track who visits or updates a node

---
+++ nodeRBT.js
@@ -0,0 +1,14 @@
+//const RED = true;
+//const BLACK = false;
+
+class NodeRBT {
+    constructor(key, value){
+        this.key = key
+        this.value = value
+        this.left = null
+        this.right = null
+        this.colour = true
+    }
+}
+
+module.exports = NodeRBT;
