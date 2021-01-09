a2e9527d526da36ac1e2755c1439e6ed95b45f39
Patricio Zarauz <pwzarauz@gmail.com>
2021-01-09 17:10:55 -0300

    Added a users field to the class
    This was added inorder to keep track who last updated or read the node

--- node.js
+++ node.js
@@ -1,3 +1,5 @@
+const NodeRBT = require('./nodeRBT');
+const RBT = require('./rbt');
 /**
  * Node is the class being stored as the value in the memcached cache.
  * @param {string} Node.key - The key of the node.
@@ -6,17 +8,22 @@
  * @param {string} Node.bytes - The bytes of the node.
  * @param {string} Node.datablock - The datablock of the node.
  * @param {Number} Node.timeout - The timeout id of the node.
+ * @param {nodeRBT} Node.users - A tree of users that visited or last modified the node.
  * @param {Node} Node.next - The following node, inorder to form an LRU.
  * @param {Node} Node.prev - The previous node, inorder to form an LRU.
  */
 class Node {
-    constructor (key, flags, exptime, bytes, datablock, next= null, prev=null){
+    constructor (key, flags, exptime, bytes, datablock, user, next= null, prev=null){
         this.key = key
         this.flags = flags
         this.exptime = exptime
         this.bytes = bytes
         this.datablock = datablock
         this.timeOut = null
+
+        this.users = new RBT()
+        this.users.add(user, true)
+
         this.next = next
         this.prev = prev
     }
