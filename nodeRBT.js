9d41b214bf2e91e31c06dde66ee35af0aab5ec1d
Patricio Zarauz <pwzarauz@gmail.com>
2021-01-13 01:41:03 -0300

    Updated comments and added a parent field to the class

--- nodeRBT.js
+++ nodeRBT.js
@@ -1,23 +1,28 @@
 class NodeRBT {
 	/**
 	 * NodeRBT is the class being stored in the RBT.
-	 * @param {Number} key - The key of the node.
-	 * @param {Boolean} value - The value of the node.
-	 * @param {Boolean} colour - The colour of the node.
+	 * @param {Number} key - Key of the node.
+	 * @param {Boolean} value - Value of the node.
+	 * @param {Boolean} colour - Colour of the node.
 	 *                                      Black = false
 	 *                                      Red   = true
-	 * @param {NodeRBT} left - The right child of the node.
-	 * @param {NodeRBT} right - The left child of the node.
+	 * @param {NodeRBT} parent - Parent of the node.
+	 * @param {NodeRBT} left - Right child of the node.
+	 * @param {NodeRBT} right - Left child of the node.
 	 */
 	constructor(key, value) {
 		this.key = key;
 		this.value = value;
 		/**
-		 * @type {NodeRBT} - The left child of the node.
+		 * @type {NodeRBT} - Parent of the node.
+		 */
+		this.parent = null;
+		/**
+		 * @type {NodeRBT} - Left child of the node.
 		 */
 		this.left = null;
 		/**
-		 * @type {NodeRBT} - The right child of the node.
+		 * @type {NodeRBT} - Right child of the node.
 		 */
 		this.right = null;
 		this.colour = true;
